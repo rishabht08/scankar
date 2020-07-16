c43e457e7091465bcce7789f662e56d3f9568ba3
Rishab <rishabht123@gmail.com>
2020-07-16 05:43:46 +0530

    Final

--- src/customer/components/CategoryManuOrdersView/CategoryManuOrdersView.js
+++ src/customer/components/CategoryManuOrdersView/CategoryManuOrdersView.js
@@ -7,20 +7,16 @@ export class CategoryManuOrdersView extends Component {
     return (
       <div className="main-container">
         <div className="manu-title">
-          <h1>Your Menu</h1>
+        <h1 style={{ color: "#071a52" }}>Your Manu</h1>
         </div>
 
         <div className="second-container">
           <div id="c-grid" className="grid-container">
             <div className="location-image bg1" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
               <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
+               
               >
-                Name
+                BURGER
               </h2>
               <div
                 classn="ui star rating"
@@ -29,102 +25,46 @@ export class CategoryManuOrdersView extends Component {
               ></div>
             </div>
             <div className="location-image bg2" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
-              >
-                Name
-              </h2>
+            <h2>SOUP</h2>
             </div>
             <div className="location-image bg3" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
-              >
-                Name
-              </h2>
+            <h2>SOFT DRINK</h2>
             </div>
             <div className="location-image bg4" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
-              >
-                Name
-              </h2>
+            <h2>BEER</h2>
             </div>
             <div className="location-image bg5" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
-              >
-                Name
-              </h2>
+            <h2>PIZZA</h2>
             </div>
             <div className="location-image bg6" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
-              <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
-              >
-                Name
-              </h2>
+            <h2>DOUNT</h2>
             </div>
             <div className="location-image bg7" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
               <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
+                
               >
-                Name
+                SOUTH INDIAN 
               </h2>
             </div>
             <div className="location-image bg8" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
               <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
+                
               >
-                Name
+                NORTH INDIAN 
               </h2>
             </div>
             <div className="location-image bg9" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
               <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
+                
               >
-                Name
+                ITALYAN FOOD
               </h2>
             </div>
             <div className="location-image bg10" onClick={()=> window.location.href = `/${this.props.params.id}/custom-order`}>
               <h2
-                style={{
-                  color: "white",
-                  textAlign: "center",
-                  marginTop: "4em",
-                }}
+               
               >
-                Name
+                VEGETARIYAN
               </h2>
             </div>
           </div>
